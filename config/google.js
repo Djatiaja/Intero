f5938adefaef3831ea7f43a891e46f05956f74ca
Djatiaja <djamgt23@gmail.com>
2025-05-31 17:16:51 +0700

    deploy vercel

--- config/google.js
+++ config/google.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis');
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.REDIRECT_URI || 'http://localhost:3000/auth/google/callback'
+  process.env.REDIRECT_URI 
 );
 
 module.exports = oauth2Client;
